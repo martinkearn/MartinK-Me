b893f32119187df67d8fb4fdf610b144bc7ae1bb
Martin Kearn <martin.kearn@microsoft.com>
2016-05-23 12:10:51 +0100

    Edited layout for resource groups

--- MartinKRC2/Controllers/ArticlesController.cs
+++ MartinKRC2/Controllers/ArticlesController.cs
@@ -2,14 +2,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
 
 namespace MartinKRC2.Controllers
 {
     public class ArticlesController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var feedUrl = "https://blogs.msdn.microsoft.com/martinkearn/feed/";
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(feedUrl);
+                var responseMessage = await client.GetAsync(feedUrl);
+                var responseString = await responseMessage.Content.ReadAsStringAsync();
+            }
+
+
             return View();
         }
     }
