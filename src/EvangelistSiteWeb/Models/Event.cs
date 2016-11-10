fa4cd798d9aa1da67a2d91611f493c23dbfdf17e
Martin Kearn <martin.kearn@microsoft.com>
2016-11-10 14:30:22 +0000

    Added event model

---
+++ src/EvangelistSiteWeb/Models/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EvangelistSiteWeb.Models
+{
+    public class Event
+    {
+        public string Title { get; set; }
+        public string Group { get; set; }
+        public DateTime Date { get; set; }
+        public string City { get; set; }
+        public string ExternalLink { get; set; }
+        public string InternalLink { get; set; }
+    }
+}
