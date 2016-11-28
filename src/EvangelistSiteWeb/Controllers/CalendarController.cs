172619e51af7fff3ffc7a8ff4de117a75526b980
Martin Kearn <martin.kearn@microsoft.com>
2016-11-28 20:53:22 +0000

    Basic event front page

---
+++ src/EvangelistSiteWeb/Controllers/CalendarController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EvangelistSiteWeb.Models;
+using Microsoft.Extensions.Options;
+using EvangelistSiteWeb.Data;
+using Microsoft.EntityFrameworkCore;
+using EvangelistSiteWeb.Models.CalendarViewModels;
+
+namespace EvangelistSiteWeb.Controllers
+{
+    public class CalendarController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CalendarController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var events = await _context.Event
+                .Where(o => o.Date > DateTime.Now)
+                .ToListAsync();
+
+            var vm = new IndexViewModel()
+            {
+                Events = events
+            };
+
+            return View(vm);
+        }
+    }
+}
