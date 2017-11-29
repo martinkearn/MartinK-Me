755877ceecd74264e74500da6b8f4b51691ae798
Martin Kearn <martin.kearn@microsoft.com>
2017-11-29 13:36:11 +0000

    AppSettings options integration

--- EvangelistSite/Startup.cs
+++ EvangelistSite/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using EvangelistSite.Models;
 
 namespace EvangelistSite
 {
@@ -21,6 +22,10 @@ namespace EvangelistSite
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOptions();
+            services.Configure<PersonaliseOptions>(
+                options => Configuration.GetSection("Personalise").Bind(options));
+
             services.AddMvc();
         }
 
