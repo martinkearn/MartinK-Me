8de17dd716be18a55804185410cffb1ad672b5f5
Martin Kearn <martin.kearn@microsoft.com>
2018-10-05 14:45:05 +0100

    Ported all content from old site

---
+++ MartinKMe/Models/TalksViewModels/EditViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MartinKMe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MartinKMe.Models.TalksViewModels
+{
+    public class EditViewModel
+    {
+        public Talk Talk { get; set; }
+
+        public IEnumerable<Resource> Resources { get; set; }
+    }
+}
