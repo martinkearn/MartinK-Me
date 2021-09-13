fbaac13b207e3e670ee167811643afe4dffeb8ac
Martin Kearn <martin.kearn@microsoft.com>
2021-09-13 12:34:03 +0100

    Function startup and project structure

---
+++ MartinKMe.Functions/Startup.cs
@@ -0,0 +1,20 @@
+﻿using MartinKMe.Domain.Interfaces;
+using MartinKMe.Services;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace MartinKMe.Functions
+{
+    public class Startup : FunctionsStartup
+    {
+        /// <inheritdoc />
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            _ = builder ?? throw new ArgumentNullException(nameof(builder));
+            var services = builder.Services;
+            builder.Services.AddSingleton<IUtilityService, UtilityService>();
+            services.BuildServiceProvider();
+        }
+    }
+}
