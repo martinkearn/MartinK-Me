6d307e30b6a3564b043a2307b850099980a899e3
Martin Kearn <martin.kearn@microsoft.com>
2021-09-16 13:45:35 +0100

    Using UtilityService

--- MartinKMe.Functions/Activities/GetFileContentsActivity.cs
+++ MartinKMe.Functions/Activities/GetFileContentsActivity.cs
@@ -1,14 +1,34 @@
-﻿using Microsoft.Azure.WebJobs;
+﻿using MartinKMe.Domain.Interfaces;
+using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace MartinKMe.Functions.Activities
 {
-    internal class GetFileContentsActivity
+    public class GetFileContentsActivity
     {
-        [FunctionName(nameof(GetFileContentsActivity))]
-        public static string GetFileContents([ActivityTrigger] string input)
+        private readonly IHttpClientFactory clientFactory;
+        private readonly IUtilityService utilityService;
+
+        public GetFileContentsActivity(IUtilityService utilityService)//IHttpClientFactory httpClientFactory, 
         {
-            return $"Hello from activity {input}!";
+            //clientFactory = httpClientFactory;
+            this.utilityService = utilityService;
+        }
+
+        [FunctionName(nameof(GetFileContentsActivity))]
+        public async Task<string> GetFileContents([ActivityTrigger] string input)
+        {
+            //var client = clientFactory.CreateClient();
+            //var response = await client.GetAsync(input);
+            //response.EnsureSuccessStatusCode();
+
+            //var contents = utilityService.Base64Decode(await response.Content.ReadAsStringAsync());
+            var contents = utilityService.Base64Decode("Zm9v");
+
+            return contents;
         }
     }
 }
--- MartinKMe.Functions/Startup.cs
+++ MartinKMe.Functions/Startup.cs
@@ -1,11 +1,9 @@
 ﻿using MartinKMe.Domain.Interfaces;
 using MartinKMe.Services;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.IO;
-
+[assembly: FunctionsStartup(typeof(MartinKMe.Functions.Startup))]
 namespace MartinKMe.Functions
 {
     public class Startup : FunctionsStartup
@@ -13,20 +11,9 @@ namespace MartinKMe.Functions
         public override void Configure(IFunctionsHostBuilder builder)
         {
             _ = builder ?? throw new ArgumentNullException(nameof(builder));
-            var services = builder.Services;
             builder.Services.AddHttpClient();
             builder.Services.AddSingleton<IUtilityService, UtilityService>();
-            services.BuildServiceProvider();
-        }
-
-        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
-        {
-            FunctionsHostBuilderContext context = builder.GetContext();
-
-            builder.ConfigurationBuilder
-                .AddJsonFile(Path.Combine(context.ApplicationRootPath, "appsettings.json"), optional: true, reloadOnChange: false)
-                .AddJsonFile(Path.Combine(context.ApplicationRootPath, $"appsettings.{context.EnvironmentName}.json"), optional: true, reloadOnChange: false)
-                .AddEnvironmentVariables();
+            builder.Services.AddLogging();
         }
     }
 }
