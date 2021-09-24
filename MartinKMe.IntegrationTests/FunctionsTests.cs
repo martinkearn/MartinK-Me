371e80977026639b76888ebd985f7830418295d5
Martin Kearn <martin.kearn@microsoft.com>
2021-09-24 13:55:28 +0100

    Functions_AddArticle_CreatesBlobAndArticleEntity uploading file to Github

--- MartinKMe.IntegrationTests/FunctionsTests.cs
+++ MartinKMe.IntegrationTests/FunctionsTests.cs
@@ -1,6 +1,9 @@
 using MartinKMe.IntegrationTests.Models;
 using Microsoft.Extensions.Configuration;
+using System;
 using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 using Xunit;
@@ -37,6 +40,30 @@ namespace MartinKMe.IntegrationTests
 
             // Assert
             Assert.Equal("Content", name);
+        }
+
+        [Fact]
+        public async Task Functions_AddArticle_CreatesBlobAndArticleEntity()
+        {
+            // Arrange
+            var testRun = Guid.NewGuid();
+            var uniqueTestMessage = $"Integration test content for test run {testRun}";
+            var uniqueTestMessageBytes = Encoding.UTF8.GetBytes(uniqueTestMessage);
+            var testArticleContentsBase64 = $"LS0tCnRpdGxlOiBJbnRlZ3JhdGlvbiBUZXN0aW5nCmF1dGhvcjogTWFydGluIEtlYXJuCmRlc2NyaXB0aW9uOiBNeSB0ZXN0IGFydGljbGUKaW1hZ2U6IGh0dHBzOi8vZHVtbXlpbWFnZS5jb20vODAweDYwMC8wMDAvZmZmJnRleHQ9cGxhY2Vob2xkZXIKdGh1bWJuYWlsOiBodHRwczovL2R1bW15aW1hZ2UuY29tLzIwMHgyMDAvMDAwL2ZmZiZ0ZXh0PXBsYWNlaG9sZGVyCnR5cGU6IGFydGljbGUKc3RhdHVzOiBkcmFmdApwdWJsaXNoZWQ6IDIwMjEvMDkvMjIgMDk6MzA6MDAKY2F0ZWdvcmllczogCiAgLSBUZXN0aW5nCi0tLQoKLlRoaXMgYXJ0aWNsZSBpcyBqdXN0IGZvciB0ZXN0aW5nIHB1cnBvc2VzCgojIyBIZXJlIGlzIGEgSDIgdG8gbWFrZSBzdXJlIEhUTUwgcGFyc2luZyBpcyB3b3JraW5nCi0gQW5kCi0gSGVyZQotIEFyZQotIFNvbWUKLSBCdWxsZXRzCgojIyMgSDMKU29tZSBleHRyYSB0ZXh0{Convert.ToBase64String(uniqueTestMessageBytes)}";
+            var putBody = new ContentPutBody()
+            {
+                 Content = testArticleContentsBase64,
+                 Message = uniqueTestMessage
+            };
+            var content = new StringContent(JsonSerializer.Serialize(putBody));
+
+            // Act
+            var response = await _httpClient.PutAsync($"https://api.github.com/repos/martinkearn/content/contents/Blogs/integrationtest-{testRun}.md", content);
+            response.EnsureSuccessStatusCode();
+
+            // Assert
+            var s = "foo";
+            Assert.NotEmpty(s);
         }
     }
 }
