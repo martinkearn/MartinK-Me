d057a0507633a95dc77eceb6458ef87dbd04f9a3
Martin Kearn <martin.kearn@microsoft.com>
2023-02-07 17:12:42 +0000

    Remove boiler plate code

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,13 +1,11 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using Web.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
